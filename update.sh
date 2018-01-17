c313ba3838ed9e5f077fa30aec6bfe62096c3d59
Gogs <gogs@fake.local>
2018-01-17 23:16:52 +0700

    update sh

--- update.sh
+++ update.sh
@@ -5,6 +5,9 @@ while true; do
         echo "no change"
     else
         echo "detect changes"
+        sleep 2s
+        echo "restart ehelp"
+        pm2 restart ehelp_linux_amd64
     fi
     echo "sleep 30s"
     sleep 30s        
